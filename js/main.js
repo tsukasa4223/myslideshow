e3dc48343801a84287708fe245941f458f61f27f
Tsukasa Otsuka <tsukabou111@gmail.com>
2021-07-19 20:28:45 +0900

    jsで画像を配列で定義

---
+++ js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+{
+  // 画像を配列で定義
+  const images = [
+    'img/IMG_6118.JPG',
+    'img/IMG_2087.JPG',
+    'img/IMG_3387.JPG',
+    'img/IMG_3399.JPG',
+    'img/IMG_4901.JPG',
+    'img/IMG_4904.JPG',
+    'img/IMG_4930.JPG',
+  ];
+}
